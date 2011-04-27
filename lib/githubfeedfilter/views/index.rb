10e9c68646b939066e8ccfdf34bbbbce5283e219
Daniel Schauenberg <d@unwiredcouch.com>
2011-04-27 11:32:42 +0200

    show object_name instead of object for tags, branches

--- lib/githubfeedfilter/views/index.rb
+++ lib/githubfeedfilter/views/index.rb
@@ -22,7 +22,7 @@ class GithubFeedFilter
           ev[:action] = i["payload"]["action"] || ""
           ev[:number] = i["payload"]["number"] || ""
           ev[:repo] = i["payload"]["repo"] || i["repository"]["name"]
-          ev[:object] = i["payload"]["object"] || ""
+          ev[:object] = i["payload"]["object_name"] || ""
           begin
             ev[:followee] = i["payload"]["target"]["login"]
           rescue NoMethodError
